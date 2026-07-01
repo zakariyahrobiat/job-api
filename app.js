e313cd7cf9e0e6b120793f75f061570ec81ca090
zakariyahrobiat <zakariyahrobiat2018@gmail.com>
2026-07-01 22:28:40 +0100

    commit

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(
 );
 app.use(express.json());
 app.use(helmet());
-app.use(cors);
-app.use(xss);
+app.use(cors());
+app.use(xss());
 
 app.get("/", (req, res) => {
   res.send("Jobs API");
