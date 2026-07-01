d9d43798f51ea7e73cf868329dced6c1d99f252c
zakariyahrobiat <zakariyahrobiat2018@gmail.com>
2026-07-01 22:32:00 +0100

    commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const helmet = require("helmet");
 const cors = require("cors");
-const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
 const express = require("express");
@@ -30,7 +29,6 @@ app.use(
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-app.use(xss());
 
 app.get("/", (req, res) => {
   res.send("Jobs API");
